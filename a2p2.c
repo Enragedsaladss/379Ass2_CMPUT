a275e97e3020892640f60e95abca0453102b9552
Ryan <rgenereu@ualberta.ca>
2024-02-25 19:59:13 -0700

    Fixed most issues, need to add timing function and figure out why packet isn't sending properly

--- a2p2.c
+++ a2p2.c
@@ -27,29 +27,45 @@ typedef struct {
     char message[MAXWORD];
 } Packet;
 
+typedef struct {
+	int putLoop;
+	char arguement[MAXWORD];
+} PutPacket;
+
 void server() {
         int server_to_client_fd, client_to_server_fd;
         char buffer[BUFSIZ];
 
         mkfifo("fifo-0-1", 0666);
         mkfifo("fifo-1-0", 0666);
+	printf("Server Start.\n");
 
-        printf("Server Start.\n");
         server_to_client_fd = open(FIFO_SERVER_TO_CLIENT, O_WRONLY);
         client_to_server_fd = open(FIFO_CLIENT_TO_SERVER, O_RDONLY);
 
         while (1) {
                 Packet packet;
+		PutPacket putPacket;
                 Packet response;
                 read(server_to_client_fd, &packet, sizeof(Packet));
-
-                if (packet.type = PUT) {
+		if (packet.type == PUT) {
                         printf("Received from client: %s\n", packet.message);
-                        response.type = OK;
+			int i = 0;
+			while(i < 3) {
+				read(server_to_client_fd, &putPacket, sizeof(putPacket));
+				printf("What server sees, [%d]:%s\n", putPacket.putLoop, putPacket.arguement);
+				if(putPacket.putLoop < 0)
+					break;
+				printf("[%d]:%s\n", putPacket.putLoop, putPacket.arguement);
+				i++;
+			};
+			
+			response.type = OK;
                         write(server_to_client_fd, &response, sizeof(response));
-                }
+			
+		}
         }
-
+	
         close(server_to_client_fd);
         close(client_to_server_fd);
 }
@@ -72,24 +88,21 @@ void client(const char *input_file) {
                 perror("Error opening file");
                 exit(EXIT_FAILURE);
         }
-
+	
         printf("Client started.\n");
-        printf("HELLOWTF");
         while (fgets(buffer, BUFSIZ, file)) {
-                printf("hello")
+                
                 if(buffer[0] == '#' || buffer[0] == '\n')
                         continue;
 
                 int idNumber, delay_time;
                 char action[MAXWORD], commandFile[MAXWORD];
-                printf("Hello");
                 sscanf(buffer, "%d %s %s", &idNumber, action, commandFile);
-                printf("test %s", action);
-                if (idNumber != 1)
+                
+		if (idNumber != 1)
                         continue;
-                printf("test %s", action);
-                if (strcmp(action, "put") == 0) {
-                        printf("Hello\n");
+                
+		if (strcmp(action, "put") == 0) {
                         Packet packet;
                         packet.type = PUT;
                         printf("Transmitted:(src = client:1) (%s) (%s)\n", action, commandFile);
@@ -97,23 +110,32 @@ void client(const char *input_file) {
                         write(client_to_server_fd, &packet, sizeof(Packet));
                         int i = 0;
                         int j = 0;
+			
+			PutPacket putPacket;
+
                         while(i != 2) {
-                                printf("Hello1\n");
                                 fgets(buffer, BUFSIZ, file);
                                 if (buffer[0] == '{' || buffer[0] == '}')
-                                        i++;
+					i++;
                                 else    {
-                                        printf("[%d]:%s", j, buffer);
+					putPacket.putLoop = j;
+                                        strcpy(putPacket.arguement,buffer);
+					printf("What server sees, [%d]:%s\n", putPacket.putLoop, putPacket.arguement);
+					write(client_to_server_fd, &putPacket, sizeof(putPacket));
+                                        printf("[%d]:%s\n", j, buffer);
                                         j++;
                                 }
-
                         }
-                }
+			putPacket.putLoop = -1;
+                	write(client_to_server_fd, &putPacket, sizeof(putPacket));
+			printf("Server Exited %d", putPacket.putLoop);
+		}
+
                 Packet receive;
 
                 write(client_to_server_fd, buffer, strlen(buffer) + 1);
                 read(server_to_client_fd, &receive, sizeof(receive));
-                printf("Received from server: %s", receive.type);
+                printf("Received from server: %d", receive.type);
     }
 
     fclose(file);
